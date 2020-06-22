1fb50c8aaa39a8d2a2d986252d39c97a5020f866
Mangipudi Prashanth Sarma <mangipudiprashanth10@gmail.com>
2020-06-22 14:06:03 +0530

    Add Test images

--- programs-suite/maintf2.py
+++ programs-suite/maintf2.py
@@ -15,7 +15,7 @@ class FilePaths:
 	fnCharList = '../model/charList.txt'
 	fnAccuracy = '../model/accuracy.txt'
 	fnTrain = '../data/'
-	fnInfer = '../data/1.png'
+	fnInfer = '../data/test/test.png'
 	fnCorpus = '../data/corpus.txt'
 
 
